0e45e248fd734b2b9647dfc58ba51892fc27b086
Natalia Puzina <puzinanata@gmail.com>
2025-03-18 21:37:21 +0000

    WIP: script for deployment to VM

--- myproject/scripts/autodeploy_vm.py
+++ myproject/scripts/autodeploy_vm.py
@@ -6,7 +6,7 @@ command_sudo = "sudo bash"
 command_last_commit = "git log -1 --pretty=format:'%H' --no-merges"
 
 # Command to update local git repo
-command_pull = "git pull"
+command_pull = "git pull &> /dev/null"
 
 my_repo_path = "git_log_viz/myproject"
 my_repo_name = my_repo_path.split('/')[-1]
@@ -32,4 +32,4 @@ print(f"Last commit in {my_repo_name} after pulling:", last_commit_after_pull)
 if last_commit_after_pull != last_commit_before_pull:
     print(f"repo {my_repo_name} updated")
 else:
-    print(f"repo {my_repo_name} shouldn't update")
+    print(f"no changes in repo {my_repo_name}, so it didn't update")
