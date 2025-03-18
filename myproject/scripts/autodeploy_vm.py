fbc279a07271c12f75fcbe93db8019c136f13962
Natalia Puzina <puzinanata@gmail.com>
2025-03-18 21:53:43 +0000

    WIP: script for deployment to VM

--- myproject/scripts/autodeploy_vm.py
+++ myproject/scripts/autodeploy_vm.py
@@ -1,6 +1,5 @@
 import subprocess
 
-command_sudo = "sudo bash"
 
 # Command to extract data from git log
 command_last_commit = "git log -1 --pretty=format:'%H' --no-merges"
@@ -12,24 +11,24 @@ my_repo_path = "git_log_viz/myproject"
 my_repo_name = my_repo_path.split('/')[-1]
 
 last_commit_before_pull = subprocess.run(
-            f"{command_sudo}; cd {my_repo_path}; {command_last_commit}",
+            f"cd {my_repo_path} && sudo {command_last_commit}",
             shell=True,
             text=True,
             capture_output=True
         ).stdout.strip()
 
-print(f"Last commit in {my_repo_name} before pulling:", last_commit_before_pull)
+print(f"Last commit in {my_repo_name} before pulling:", last_commit_before_pull, flush=True)
 
 last_commit_after_pull = subprocess.run(
-            f"cd {my_repo_path}; {command_pull}; {command_last_commit}",
+            f"{command_pull} && {command_last_commit}",
             shell=True,
             text=True,
             capture_output=True
             ).stdout.strip()
 
-print(f"Last commit in {my_repo_name} after pulling:", last_commit_after_pull)
+print(f"Last commit in {my_repo_name} after pulling:", last_commit_after_pull, flush=True)
 
 if last_commit_after_pull != last_commit_before_pull:
-    print(f"repo {my_repo_name} updated")
+    print(f"repo {my_repo_name} updated", flush=True)
 else:
-    print(f"no changes in repo {my_repo_name}, so it didn't update")
+    print(f"no changes in repo {my_repo_name}, so it didn't update", flush=True)
