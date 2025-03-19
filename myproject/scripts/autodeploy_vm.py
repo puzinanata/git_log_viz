ba6b666c7cf68e4f6e970712ffb6c7b02f1afa73
Natalia Puzina <puzinanata@gmail.com>
2025-03-19 14:35:28 +0000

    WIP: script for deployment to VM

--- myproject/scripts/autodeploy_vm.py
+++ myproject/scripts/autodeploy_vm.py
@@ -1,17 +1,16 @@
 import subprocess
 
-
 # Command to extract data from git log
 command_last_commit = "git log -1 --pretty=format:'%H' --no-merges"
 
 # Command to update local git repo
 command_pull = "git pull &> /dev/null"
 
-my_repo_path = "git_log_viz/myproject"
+my_repo_path = "~/git_log_viz/myproject"
 my_repo_name = my_repo_path.split('/')[-1]
 
 last_commit_before_pull = subprocess.run(
-            f"cd {my_repo_path} && sudo {command_last_commit}",
+            f"sudo bash -c 'cd {my_repo_path}; {command_last_commit}'",
             shell=True,
             text=True,
             capture_output=True
@@ -19,8 +18,15 @@ last_commit_before_pull = subprocess.run(
 
 print(f"Last commit in {my_repo_name} before pulling:", last_commit_before_pull, flush=True)
 
+# Run sudo git pull (suppress output)
+subprocess.run(
+    f"sudo bash -c 'cd {my_repo_path}; {command_pull}'",
+    shell=True,
+    text=True
+)
+
 last_commit_after_pull = subprocess.run(
-            f"{command_pull} && {command_last_commit}",
+            f"sudo bash -c 'cd {my_repo_path}; {command_last_commit}'",
             shell=True,
             text=True,
             capture_output=True
