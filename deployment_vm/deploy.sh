7ba9eae4256b0fd9d73c977a7916fa4723ea62cb
Natalia Puzina <puzinanata@gmail.com>
2025-07-17 18:31:41 +0100

    WIP: improved deployment script

--- deployment_vm/deploy.sh
+++ deployment_vm/deploy.sh
@@ -13,27 +13,7 @@ set -x
 # Turn off debugging mode
 #set +x
 
-# Define global variables
-VENV_NAME="venv"
-
-# Function to activate virtual environment
-activate_venv() {
-    if [ ! -d "$VENV_NAME" ]; then
-        python3 -m venv "$VENV_NAME"
-        if [ ! -d "$VENV_NAME" ]; then
-            echo "Error: Virtual environment not created successfully."
-            exit 1
-        fi
-        echo "Virtual environment '$VENV_NAME' created."
-    fi
-
-    # Activate the virtual environment
-    source "$VENV_NAME/bin/activate"
-    echo "Virtual environment activated."
-}
-
 # Step 0: Install required packages
-set +e
 REQUIRED_PACKAGES="git
     nginx
     python3
@@ -43,10 +23,7 @@ REQUIRED_PACKAGES="git
 apt update
 
 DEBIAN_FRONTEND=noninteractive apt install -y $REQUIRED_PACKAGES
-
 echo "All required packages installed!"
-set -e
-
 
 # Step 1: Pull or clone my project
 REPO_URL="https://github.com/puzinanata/git_log_viz"
@@ -61,7 +38,9 @@ else
 fi
 
 # Step 2: Set up and activation virtual environment
-activate_venv
+python3 -m venv venv
+source venv/bin/activate
+echo "Virtual environment activated."
 
 # Step 3:
 echo "Creating directory for Git repositories: /var/lib/git_repos"
@@ -70,31 +49,19 @@ chown "$USER":"$USER" "/var/lib/git_repos"
 chmod 755 "/var/lib/git_repos"
 
 # Step 4: Install requirements
-if [ -f "deployment_vm/requirements.txt" ]; then
-    echo "Installing Python dependencies from requirements.txt..."
-    pip install -r deployment_vm/requirements.txt
-    echo "Dependencies installed successfully."
-else
-    echo "requirements.txt not found in deployment_vm/!"
-fi
+pip install -r deployment_vm/requirements.txt
+echo "Dependencies installed successfully."
 
 # Step 5: Add venv to .gitignore
-if ! grep -q "$VENV_NAME/" myproject/.gitignore 2>/dev/null; then
-    echo "$VENV_NAME/" >> myproject/.gitignore
-fi
+echo "venv/" >> myproject/.gitignore
 
 # Step 6: Run Django migrations
 echo "Navigating to the project directory..."
 cd myproject/
-
-if [ -f "manage.py" ]; then
-    echo "Running migrations and collecting static files..."
-    python3 manage.py migrate
-    python3 manage.py collectstatic --noinput
-    echo "Migrations and static files collection completed."
-else
-    echo "manage.py not found, ensure you're in the correct project directory."
-fi
+echo "Running migrations and collecting static files..."
+python3 manage.py migrate
+python3 manage.py collectstatic --noinput
+echo "Migrations and static files collection completed."
 
 # Step 7: Configure Nginx
 GITREPORT_CONF="../deployment_vm/gitreport.conf"
@@ -107,5 +74,5 @@ ln -sf "$NGINX_CONF_DEST" "$NGINX_ENABLED"
 rm -f /etc/nginx/sites-enabled/default
 nginx
 
-echo "Starting Django runserver in daemon mode..."
+echo "Starting Django runserver..."
 python3 manage.py runserver
